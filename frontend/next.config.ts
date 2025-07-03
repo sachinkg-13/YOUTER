22cf441175f86e98e26baac9c69c0685a54b9785
Sachin kumar gupta <sachinkg1309@gmail.com>
2025-07-03 22:10:51 +0530

    refactor: update frontend package.json and dependencies - Updated dependencies with new packages from Radix UI and others. - Adjusted devDependencies for TypeScript and Tailwind CSS. - Modified postcss.config.mjs to use object syntax for plugins. - Changed TypeScript target from ES2017 to ES6 and updated paths in tsconfig.json.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: false
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
